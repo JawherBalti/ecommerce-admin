b207f020b32de18df42ef4b9840b3d9da476d2aa
jawherbalti <jawher-balti@live.com>
2023-09-26 10:06:35 +0100

    add only one store

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -6,9 +6,23 @@ import { useStoreModal } from "@/hooks/use-store-modal";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { Button } from "./ui/button";
-import { Check, ChevronsUpDown, PlusCircle, Store as StoreIcon } from "lucide-react";
+import {
+  Check,
+  ChevronsUpDown,
+  PlusCircle,
+  Store as StoreIcon,
+} from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from "./ui/command";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "./ui/command";
+import toast from "react-hot-toast";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<
   typeof PopoverTrigger
@@ -18,7 +32,10 @@ interface StoreSwitcherProps extends PopoverTriggerProps {
   items: Store[];
 }
 
-export default function StoreSwitcher({ className, items = [] }: StoreSwitcherProps) {
+export default function StoreSwitcher({
+  className,
+  items = [],
+}: StoreSwitcherProps) {
   const [open, setOpen] = useState(false);
   const storeModal = useStoreModal();
   const params = useParams();
@@ -50,45 +67,54 @@ export default function StoreSwitcher({ className, items = [] }: StoreSwitcherPr
           className={cn("w-[200px] justify-between", className)}
         >
           <StoreIcon className="mr-2 h-4 w-4" />
-            {currentStore?.label}
-            <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50"/>
+          {currentStore?.label}
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0">
         <Command>
-            <CommandList>
-                <CommandInput placeholder="Search store"/>
-                <CommandEmpty>No store founds.</CommandEmpty>
-                <CommandGroup heading="Stores">
-                    {formattedItems.map(store=> (
-                        <CommandItem 
-                            key={store.value} 
-                            onSelect={()=>onStoreSelect(store)} 
-                            className="text-sm"
-                        >
-                            <StoreIcon className="mr-2 h-4 w-4"/>
-                            {store.label}
-                            <Check
-                                className={cn("ml-auto h-4 w-4", currentStore?.value === store.value ? "opacity-100" : "opacity-0")}
-                            />
-                        </CommandItem>
-                    ))}
-                </CommandGroup>
-            </CommandList>
-            <CommandSeparator/>
-            <CommandList>
-                <CommandGroup>
-                    <CommandItem
-                        onSelect={()=> {
-                            setOpen(false)
-                            storeModal.onOpen()
-                        }}
-                    >
-                        <PlusCircle className="mr-2 h-5 w-5"/>
-                        Create Store
-                    </CommandItem>
-                </CommandGroup>
-            </CommandList>
+          <CommandList>
+            <CommandInput placeholder="Search store" />
+            <CommandEmpty>No store founds.</CommandEmpty>
+            <CommandGroup heading="Stores">
+              {formattedItems.map((store) => (
+                <CommandItem
+                  key={store.value}
+                  onSelect={() => onStoreSelect(store)}
+                  className="text-sm"
+                >
+                  <StoreIcon className="mr-2 h-4 w-4" />
+                  {store.label}
+                  <Check
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      currentStore?.value === store.value
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+          <CommandSeparator />
+          <CommandList>
+            <CommandGroup>
+              <CommandItem
+                onSelect={() => {
+                  if (formattedItems.length === 0) {
+                    setOpen(false);
+                    storeModal.onOpen();
+                  } else {
+                    toast.error("Cannot add more that a store");
+                  }
+                }}
+              >
+                <PlusCircle className="mr-2 h-5 w-5" />
+                Create Store
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
         </Command>
       </PopoverContent>
     </Popover>
